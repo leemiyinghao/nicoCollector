e53f3191088378b1df8cc115f4fd8990b6184f73
catLee <leemiyinghao@gmail.com>
2014-05-23 18:15:30 +0900

    Add function Inserting data into database.sqlite

--- nicoCollector.rb
+++ nicoCollector.rb
@@ -1,7 +1,8 @@
 #!/bin/ruby
 # -*- coding: utf-8 -*-
-require 'sqlite'
+require 'sqlite3'
 class HTML
+  #TODO:Replace to a curl Webpage Grabber
   def initialize(filename)
     @file = File.open(filename)
   end
@@ -24,16 +25,20 @@ end
 class DATABASE
   #Database Connect Layer,type SQLite3
   def initialize(name)
-    @db = SQLite::Database.new(name)
+    @db = SQLite3::Database.new(name)
   end
   def query(string)
     return @db.execute(string)
+  end
+  def insert(uploadDate,smNumber,numViews,numComments,numMyList,numAds)
+    return @db.execute("INSERT INTO list (uploadDate,smNumber,numViews,numComments,numMyList,numAds) VALUES ('#{uploadDate}','#{smNumber}','#{numViews}','#{numComments}','#{numMyList}','#{numAds}');")
   end
 end
 html = HTML.new("template")
 regex = REGEX.new("regexp")
 db = DATABASE.new("database.sqlite")
+#list = {uploadDate,smNumber,numViews,numComments,numMyList,numAds,recodeDate}
 html.getContext.scan(/#{regex.getExpression}/).each {|arr|
-  myListRank = 100*Float(arr[5])/Float(arr[3])
-  #TODO:Insert data
+  #arr = {year,month,date,hour,minute,title,smNumber,comment,views,numComments,myList,ads}
+  db.insert(DateTime.new(2000+Integer(arr[0]),Integer(arr[1]),Integer(arr[2]),Integer(arr[3]),Integer(arr[4]),0,'+09:00'),arr[6],arr[8],arr[9],arr[10],arr[11])
 }
